c00d478bf5ae49ff54c8b117792e493040ddff6c
Dev Kathuria <dk382@snu.edu.in>
2019-07-23 16:38:02 +0530

    animation

--- main.js
+++ main.js
@@ -1,18 +1,4 @@
 $(document).ready(function() {
-    var banner_height = $("#responsive-navbar").height();
-    var lastScrollTop = 0;
-    $(window).scroll(function() {
-        var scroll = $(window).scrollTop();
-        var currScrollTop = $(this).scrollTop();
-        if (scroll >= banner_height && currScrollTop > lastScrollTop) {
-            $("#responsive-navbar").hide("fast");
-        } else {
-            $("#responsive-navbar").show("fast");
-        }
-        lastScrollTop = currScrollTop;
-
-    });
-
 
     $("#arr-img").hover(function(){
         $(this).toggleClass("bounce2");
