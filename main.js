6a4f13323be511cb8447ece9cca9f81260194e9e
Dev Kathuria <dk382@snu.edu.in>
2019-07-27 11:16:43 +0530

    firebase added for contact us form

--- main.js
+++ main.js
@@ -20,11 +20,30 @@ $(document).ready(function() {
             form1.name.value='';
             form1.email.value='';
             form1.desc.value='';
-
-            // window.location='thanks.html';
+            
         });
     }
     else console.log(form1);
     //form submit ends
+
+    //contact us form
+    const form2 = document.getElementById("formcontactus");
+    if(form2){
+        form1.addEventListener('submit',function (e) {
+            e.preventDefault();
+            db.collection('contactus').add({
+                name:form2.name.value,
+                mob:form2.mob.value,
+                email:form2.email.value,
+                desc:form2.desc.value,
+            });
+            form2.name.value='';
+            form2.mob.value='';
+            form2.email.value='';
+            form2.desc.value='';
+        });
+    }
+    else console.log(form1);
+
 
 });
