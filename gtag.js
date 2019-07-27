50346e5ab431c5b2a56ff2ba0b212d2341ab0d8f
Dev Kathuria <dk382@snu.edu.in>
2019-07-27 11:36:56 +0530

    analytics bug fix

--- gtag.js
+++ gtag.js
@@ -1,9 +1,2 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-144663500-1"></script>
-<script>
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
 
-gtag('config', 'UA-144663500-1');
-</script>
